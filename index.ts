78a7d968fc67453b3927b81294dd659c9762eacb
Tomas Jansson <tomas.jansson@gmail.com>
2021-11-24 00:39:14 +0100

    Basic app done

--- index.ts
+++ index.ts
@@ -3,3 +3,6 @@ import * as pulumi from "@pulumi/pulumi";
 const config = new pulumi.Config();
 const message = config.get('message');
 export { message };
+
+import { ResourceGroup } from "@pulumi/azure-native/resources";
+const resourceGroup = new ResourceGroup('ndc-rg', {});
